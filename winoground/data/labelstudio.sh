164f3c9e3098b09d756cc07e70bad1e7c0769758
Julen Etxaniz <juletxara@gmail.com>
2022-09-19 15:37:37 +0200

    update label studio config

--- winoground/data/labelstudio.sh
+++ winoground/data/labelstudio.sh
@@ -1,3 +1,4 @@
+source ../../venv3.8.6/bin/activate
 export LOCAL_FILES_SERVING_ENABLED=true
 export LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT=/gaueko0/users/jetxaniz007/spatial-reasoning/winoground
 label-studio start
